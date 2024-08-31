d0e10b092bf71f34dde5e5d52f868adecdb61849
rofioy <rofisyarifhidayat@gmail.com>
2024-09-01 02:06:56 +0700

    commit

--- index.js
+++ index.js
@@ -1,18 +1,15 @@
-// Dark Mode
-// On page load or when changing themes, best to add inline in `head` to avoid FOUC
+//Dark Mode toggle
 if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
-  document.documentElement.classList.add('dark')
+  document.documentElement.classList.add('dark');
 } else {
-  document.documentElement.classList.remove('dark')
+  document.documentElement.classList.remove('dark');
 }
 
 // Whenever the user explicitly chooses light mode
-localStorage.theme = 'light'
+localStorage.theme = 'light';
 
-// Whenever the user explicitly chooses dark mode
-localStorage.theme = 'dark'
+// Whenever the user explicitly hooses dark mode
+localStorage.theme = 'dark';
 
 // Whenever the user explicitly chooses to respect the OS preference
-localStorage.removeItem('theme')
-
-
+localStorage.removeItem('theme');
